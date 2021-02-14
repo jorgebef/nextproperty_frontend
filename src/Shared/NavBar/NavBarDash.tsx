4feeabee5813dfb4ebfea45f637789f12af16f4b
Jorge Befan <jorge.befan@gmail.com>
2021-02-14 11:55:31 +0100

    upload to server

--- src/Shared/NavBar/NavBarDash.tsx
+++ src/Shared/NavBar/NavBarDash.tsx
@@ -63,14 +63,13 @@ export const NavBarDash = (): React.ReactElement => {
                 }`}
         >
             <ul className={style.navbar_nav}>
-                <Link to='/dashboard/list' >
-                    <a
-                        href="http://localhost:3000/dashboard/list"
-                        className={style.logo_link}
-                    >
-                        NextProperty Control Panel
-                    </a>
+                <Link to='/dashboard/list'
+                    className={style.logo_link}
+                >
                 </Link>
+
+                    NextProperty Control Panel
+
                 <li className={style.links_item}>
                     <Link to="/" className={style.nav_link}>
                         <span>Home</span>
